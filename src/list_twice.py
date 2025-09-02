3c40a31d4b3621732ff8973da8d8a31ab35ea8ed
26blevine <26blevine@gannacademy.org>
2025-09-02 15:35:59 -0400

    ! causes pain

--- src/list_twice.py
+++ src/list_twice.py
@@ -1,3 +1,13 @@
 # Write your solution here
 list = []
+i = int(input("New item: "))
+while i != 0:
+    
+    list.append(i)
+    print(f"The list now: {list}")
+    print(f"The list in order: {sorted(list)}")
+    i = int(input("New item: "))
+
+print("Bye!")
+
 
